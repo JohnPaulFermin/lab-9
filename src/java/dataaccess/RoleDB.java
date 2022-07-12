88df46f5206ffe304a208ae8fe45f74fc6c6ec40
Matchstix06 <jaypee_fermin@yahoo.ca>
2022-07-11 22:31:13 -0600

    lab 9 recommit

---
+++ src/java/dataaccess/RoleDB.java
@@ -0,0 +1,28 @@
+package dataaccess;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import models.Role;
+import models.User;
+/**
+ *
+ * @author Alex Tompkins - 821984
+ */
+public class RoleDB {
+    public List<Role> getAll() throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        try {
+            List<Role> role = em.createNamedQuery("Role.findAll",Role.class).getResultList();
+            return role;
+        }finally{
+            em.close();
+    }
+        
+    }
+    
+            
+}
